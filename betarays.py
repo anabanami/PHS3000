bb79d1bb6683f4e397490cd06ab5fe6c1690995e
anabanami <ana.fabelah@gmail.com>
2020-08-15 16:13:12 +1000

    PREPARED FOR ROUND 2

--- betarays.py
+++ betarays.py
@@ -5,26 +5,40 @@ import pytz
 import matplotlib.pyplot as plt
 plt.rcParams['figure.dpi'] = 150
 
-data = spa.betaray.read_data(r'betaray_data.csv')
+data = spa.betaray.read_data(r'betaray_data2.csv')
 
-i = 0
-for run in data:
-    # print(f'{i=}')
-    if run[0] == pd.Timestamp('2020-08-06 13:39:36+10:00', tz=pytz.FixedOffset(600)):
-        valid_data = data[i:]
-        continue
-    i+=1
+# GLOBALS
+# physical constants and SI units
+hbar = 1.0545718e-34 # [Js]
+c = 299792458 # [m/s]
+epsilon_0 = 8.854e-12 
+mass_e = 9.10938356e-31 # [kg]
+e_charge = 1.60217662e-19 # [C]
+eV = 1.602176634e-19 # [J]
+meV = 1e-3 * eV
+keV = 1e6 * eV
+angstrom = 1e-10 # [m]
+
+
+T_K = 624.21 * keV
+
+w = T + mass_e * c**2
+
+# i = 0
+# for run in data:
+#     # print(f'{i=}')
+#     if run[0] == pd.Timestamp('2020-08-06 13:39:36+10:00', tz=pytz.FixedOffset(600)):
+#         valid_data = data[i:]
+#         continue
+#     i+=1
 
 count = []
 lens_current = []
-for row in valid_data:
+for row in data:
     count.append(row[5])
     lens_current.append(row[6])
 
-
-
-
-plt.figure(1)
+plt.figure(2)
 # # plot
 plt.plot(
         lens_current, count, marker="None",
@@ -36,7 +50,7 @@ plt.title(r"$\beta^{-}$ particle count as a function of lens coil current")
 plt.xlabel("I [A]")
 plt.ylabel("N(I) [arb]")
 plt.legend()
-spa.savefig('betarays_fig1.png')
+spa.savefig('betarays_fig2.png')
 plt.show()
 
 # print(f'{count=}')
